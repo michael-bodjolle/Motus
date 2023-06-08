4add15d2a704bb9185624d69c2842b7377f3cd58
michael <michael.bodjolle@laplateforme.io>
2023-06-08 17:09:16 +0200

    jsonnewvers

--- motus.js
+++ motus.js
@@ -1,52 +1,46 @@
-import data from './mots.json' assert {type: 'json'};
+import data from "./test.json" assert { type: "json" };
 
 export default class Motus {
   constructor() {
-    this.init()
+    this.init();
   }
   init() {
-    this.words = data
-    this.drawBoard()
-    this.getWord()
+    this.words = data;
+    const i = Math.floor(Math.random() * data.length - 1) + 1;
+    this.word = data[i];
+    this.drawBoard();
+    this.getWord();
   }
-  
-  drawBoard() {
 
-    const board = document.getElementById('board')
-    console.log(board)
+  drawBoard() {
+    const board = document.getElementById("board");
+    console.log(board);
 
     for (let i = 0; i < 6; i++) {
+      
+
       const row = document.createElement("div");
 
-      row.setAttribute("id", `row`)
-      row.classList.add(`row${i}`)
-
-
-      board.appendChild(row)
-
-      for (let j = 0; j < this.words[0].mot.length; j++) {
-
+      row.setAttribute("id", `row`);
+      row.classList.add(`row${i}`);
+      
+      board.appendChild(row);
+       
+      for (let j = 0; j < this.word.length; j++) {
         const Cellule = document.createElement("div");
+        if (i == 0 && j == 0) {
+          Cellule.append(this.word[0]);
+        }
 
-        row.appendChild(Cellule)
+        row.appendChild(Cellule);
 
-        Cellule.setAttribute("id", 'cell')
-        Cellule.classList.add(`Cellule${j}`)
+        Cellule.setAttribute("id", "cell");
+        Cellule.classList.add(`Cellule${j}`);
       }
-
-
     }
-    
-
   }
 
-  getWord(){
-  const words = data
-  console.log(words[0].mot.length)
+  getWord() {
+    console.log(this.word[0]);
   }
-
-
-
 }
-
-
